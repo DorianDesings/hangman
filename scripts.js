14c23d98b9b7821a0d7b4d3ad9cda66e5d0a3689
DorianDesings <dorianoriginaldesings@gmail.com>
2020-08-31 21:16:27 +0200

    Directo terminando Hangman parte 1

--- scripts.js
+++ scripts.js
@@ -5,6 +5,17 @@ const popUp = document.getElementById('pop-up');
 const popUpText = document.getElementById('pop-up-text');
 const popUpButton = document.getElementById('pop-up-button');
 
+const scoreText = document.getElementById('score-text');
+const scoreLosse = document.getElementById('score-losses');
+const scoreWins = document.getElementById('score-wins');
+
+const ls = localStorage;
+
+const games = {
+  wins: 0,
+  losses: 0
+};
+
 const words = ['ordenador', 'javascript'];
 const letters = [
   'A',
@@ -35,13 +46,48 @@ const letters = [
   'Z'
 ];
 
-const correctLetters = [];
-const maxAttempts = 6;
+let correctLetters = [];
+const maxAttempts = 5;
 let attempts = 0;
+let totalPoints = 0;
 
-const selectedWord = words[
-  Math.floor(Math.random() * words.length)
-].toUpperCase();
+const writeInLocalStorage = (wins = 0, looses = 0) => {
+  if (localStorage.getItem('totalScore')) {
+    const totalScoreLS = JSON.parse(localStorage.getItem('totalScore'));
+    games.wins = totalScoreLS.wins;
+    games.losses = totalScoreLS.losses;
+  } else {
+    localStorage.setItem('totalScore', JSON.stringify(games));
+  }
+};
+
+//TODO terminar función para el localstorage
+
+const getRandomWord = () =>
+  words[Math.floor(Math.random() * words.length)].toUpperCase();
+
+let selectedWord = getRandomWord();
+
+const showPopUp = win => {
+  popUp.classList.add('pop-up--show');
+  popUpText.textContent = win ? 'HAS GANADO' : 'HAS PERDIDO';
+  popUpButton.textContent = 'JUGAR OTRA VEZ';
+};
+
+const scoreMaster = points => {
+  totalPoints += points;
+  scoreText.textContent = `Score: ${totalPoints}`;
+};
+
+const resetGame = () => {
+  correctLetters = [];
+  selectedWord = getRandomWord();
+  writeWord();
+  removeUsedLetters();
+  popUp.classList.remove('pop-up--show');
+  popUpText.textContent = '';
+  popUpButton.textContent = '';
+};
 
 const writeWord = () => {
   const wordToWrite = selectedWord
@@ -57,14 +103,10 @@ const writeWord = () => {
 
   wordElement.innerHTML = wordToWrite;
 
-  // const wordIngame = wordElement.innerText.replace(/\n\s/g, '');
-
   const wordIngame = wordElement.textContent.replace(/\s+/g, '').trim();
 
   if (wordIngame === selectedWord) {
-    popUp.classList.add('pop-up--show');
-    popUpText.textContent = 'HAS GANADO';
-    popUpButton.textContent = 'JUGAR OTRA VEZ';
+    showPopUp(true);
   }
 };
 
@@ -85,6 +127,9 @@ const updateWrongAttempts = () => {
   hangmanParts[attempts].classList.remove('hangman__part');
   if (attempts < maxAttempts) {
     attempts += 1;
+  } else {
+    showPopUp(false);
+    attempts = 0;
   }
 };
 
@@ -93,9 +138,11 @@ const checkLetter = letter => {
     if (!correctLetters.includes(letter)) {
       correctLetters.push(letter);
       writeWord();
+      scoreMaster(10);
     }
   } else {
     updateWrongAttempts();
+    scoreMaster(-5);
   }
 };
 
@@ -103,6 +150,12 @@ const markUsedLetters = letter => {
   document
     .querySelector(`[data-letter=${letter.toUpperCase()}]`)
     .classList.add('keyboard__key--used');
+};
+
+const removeUsedLetters = () => {
+  const allKeys = [...document.querySelectorAll('.keyboard__key')];
+
+  allKeys.forEach(key => key.classList.remove('keyboard__key--used'));
 };
 
 window.addEventListener('keyup', e => {
@@ -117,5 +170,11 @@ keyboard.addEventListener('click', e => {
   }
 });
 
+popUpButton.addEventListener('click', () => {
+  hangmanParts.forEach(part => part.classList.add('hangman__part'));
+  resetGame();
+});
+
 writeWord();
 writeKeyboard();
+scoreMaster(0);
